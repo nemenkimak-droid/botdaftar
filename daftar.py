eb0854db484da004aecf2e85abe6b3f55cac6ae1
nemenkimak-droid <nemenkimak@gmail.com>
2026-06-12 16:28:55 +0700

    Update daftar.py

--- daftar.py
+++ daftar.py
@@ -38,7 +38,7 @@ timezone = pytz.timezone(os.environ.get("TZ", "Asia/Jakarta"))
 # ----------------------
 # TARGET ID SPESIFIK DARI LINK TELEGRAM
 TARGET_CHAT_ID = -1001832259904
-TARGET_THREAD_ID = 376193
+TARGET_THREAD_ID = 3785049591/2
 
 SUBMENUS = ["DWT", "BG", "DWL", "NG", "TG88", "TTGL", "KTT", "TTGG"]
 PAGE_SIZE = 10
